6ae7e860561c30ee48e00b6ee31f40dae09a2af2
SamyInfo5 <106281630+SamyInfo5@users.noreply.github.com>
2023-01-10 22:00:09 +0100

    Finition

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 
 from datetime import datetime
 from flask import Flask, render_template, request, flash, url_for, redirect
+import uuid
+
+now = datetime.now() # current date and time
+date_time = now.strftime("%d/%m/%Y")
 
 # ...
 DATAS  =  [
@@ -9,18 +13,21 @@ DATAS  =  [
             "title":"Titre  1",
             "content":"Contenu  1",
             "category":"ecole",
+            "edit_date":date_time,
         },
         {   
             "id":"fe85ef10-9bda-4869-b609-6d603eb18847",
             "title":"Titre  2",
             "content":"Contenu  2",
             "category":"travail",
+            "edit_date":date_time,
         },
         {   
             "id":"881479b1-8b08-425b-add6-db1899acb259",
             "title":"Titre  3",
             "content":"Contenu  4",
             "category":"maison",
+            "edit_date":date_time,
         } 
      
      ] 
@@ -30,11 +37,19 @@ def search_to_do(id):
         if id == data["id"]:
             return data
 
-def delete_todo():
+def delete_todo(id):
     for data in DATAS:
         if id ==data["id"]:
-            del data["id"] 
-            return data          
+            DATAS.remove(data)    
+            return True
+
+def edit_toDo(id,title,content,category):
+    for data in DATAS:
+        if id == data["id"]:
+            data["title"]=title
+            data["content"]=content
+            data["category"]=category
+            return True
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '03237566636ed31610a390d4f455d949045b5d2323363317'
@@ -43,13 +58,14 @@ app.config['SECRET_KEY'] = '03237566636ed31610a390d4f455d949045b5d2323363317'
 
 @app.route("/")
 def liste():
-    now = datetime.now() # current date and time
-    date_time = now.strftime("%d/%m/%Y")
 
     return render_template('table.html', date_time=date_time, data=DATAS)
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%d/%m/%Y")
 
     if request.method == 'POST':
         title = request.form['title']
@@ -63,11 +79,10 @@ def create():
         elif not category:
             flash('catégory non saisi')   
         else:
-            DATAS.append({'title': title, 'content': content,'category' : category})
+            DATAS.append({"id":str(uuid.uuid4()),'title': title, 'content': content,'category' : category,'edit_date': date_time})
             return redirect(url_for('liste'))
 
-    now = datetime.now() # current date and time
-    date_time = now.strftime("%d/%m/%Y")        
+            
         
         
 
@@ -81,11 +96,21 @@ def edit(id):
         title = request.form['title']
         content = request.form['content']
         category = request.form['category']
+        print(title)
+        print(content)
+        print(category)
+        edit_toDo(id,title,content,category)
+
+
+        return render_template('table.html', data=DATAS)
 
     if request.method == 'GET':
-        title = request.args['title']
-        content = request.args['content']
-        category = request.args['category']
 
         return render_template('edit.html', onedata = search_to_do(id))
+
+@app.route("/delete/<id>") 
+def delete(id):
+    print(delete_todo(id))
+
+    return render_template('table.html', data=DATAS)   
 
